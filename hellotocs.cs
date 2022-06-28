2ee9035fc924f206e4b11da284e900fb8a7af00b
Sakhawat Hossain <sakhawatrodro@gmail.com>
2022-06-28 12:20:52 +0600

    my first program

---
+++ hellotocs.cs
@@ -0,0 +1,14 @@
+// my first program in csharp
+
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");    
+    }
+  }
+}
